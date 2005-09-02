d8922bffef83b65f3f9ff18d50b6739a5cc72960
jchiang <>
2005-09-02 06:43:05 +0000

    attempt to create setup script for windows

--- python/package_paths.py
+++ python/package_paths.py
@@ -1,19 +1,36 @@
 """
 Package-dependent shared libraries and environment variables that will
-be added to python_setup.[c]sh by SwigPolicy/v*/src/startup_scripts.py.
+be added to python_setup.([c]sh, bat) by
+SwigPolicy/v*/src/startup_scripts.py.
 
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/package_paths.py,v 1.1 2005/08/30 21:19:54 jchiang Exp $
 #
+
+import os
+
+if os.name == 'posix':
+    inst_dir = '${INST_DIR}'
+    caldb = '${CALDB}'
+else:
+    inst_dir = '%INST_DIR%'
+    caldb = '%CALDB%'
 
 from swig_setup import packageroot
 
-stlibs = ['st_graph', 'optimizers', 'xmlBase']
+stlibs = ['st_graph', 'optimizers', 'xmlBase', 'Likelihood', 'evtbin',
+          'dc1Response', 'g25Response', 'testResponse', 'tip']
 
-extra_paths = [('LIKELIHOODROOT', '${INST_DIR}/%s'%packageroot('Likelihood')),
-               ('EVTBINROOT', '${INST_DIR}/%s' % packageroot('evtbin')),
-               ('CALDB', '${INST_DIR}/irfs/%s/CALDB' % packageroot('caldb')), 
-               ('CALDBCONFIG', '${CALDB}/software/tools/caldb.config'),
-               ('CALDBALIAS', '${CALDB}/software/tools/alias_caldb.fits')]
+extra_paths = [('LIKELIHOODROOT',
+                os.path.join(inst_dir, '%s' % packageroot('Likelihood'))),
+               ('EVTBINROOT',
+                os.path.join(inst_dir, '%s' % packageroot('evtbin'))),
+               ('CALDB',
+                os.path.join(inst_dir, 'irfs',
+                             '%s' % packageroot('caldb'), 'CALDB')), 
+               ('CALDBCONFIG',
+                os.path.join(caldb, 'software', 'tools', 'caldb.config')),
+               ('CALDBALIAS',
+                os.path.join(caldb, 'software', 'tools', 'alias_caldb.fits'))]
