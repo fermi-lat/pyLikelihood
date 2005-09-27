83f9765d55bd00ba9c1a4e00ca6e1636619d1b6f
jchiang <>
2005-09-27 15:03:30 +0000

    allow sequences of parameters to passed to freeze/thaw

--- python/AnalysisBase.py
+++ python/AnalysisBase.py
@@ -4,7 +4,7 @@ Base clase for Likelihood analysis Python modules.
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/users/jchiang/pythonModules/pyLikelihood/python/AnalysisBase.py,v 1.8 2005/08/19 04:11:37 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/AnalysisBase.py,v 1.1.1.1 2005/08/22 16:19:27 jchiang Exp $
 #
 
 import numarray as num
@@ -118,6 +118,14 @@ class AnalysisBase(object):
     def __repr__(self):
         return self._inputs
     def thaw(self, i):
-        self.model[i].setFree(1)
+        try:
+            for ii in i:
+                self.model[ii].setFree(1)
+        except TypeError:
+            self.model[i].setFree(1)
     def freeze(self, i):
-        self.model[i].setFree(0)
+        try:
+            for ii in i:
+                self.model[ii].setFree(1)
+        except TypeError:
+            self.model[i].setFree(0)
