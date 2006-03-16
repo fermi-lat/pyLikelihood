1a826eb545cff166c4f6e7aaf177c19ffb59492e
cvs2git <>
2006-03-16 16:35:34 +0000

    This commit was manufactured by cvs2svn to create tag 'v0r1p3'.
    Sprout from GLAST 2006-03-16 16:35:33 UTC jchiang 'AutoUpdate'
    Cherrypick from master 2006-03-16 16:35:32 UTC jchiang 'tagging v0r1p3':
        cmt/requirements
        doc/images/hippo_plot_2.png
        doc/release.notes
        python/AnalysisBase.py
        python/BinnedAnalysis.py
        python/SrcModel.py
        python/UnbinnedAnalysis.py
        python/package_paths.py
        src/pyLikelihood.in
    Delete:
        python/AnalysisBase.pyc
        python/BinnedAnalysis.pyc
        python/HippoPlot.pyc
        python/SimpleDialog.pyc
        python/SrcModel.pyc

--- python/AnalysisBase.py
+++ python/AnalysisBase.py
@@ -4,28 +4,37 @@ Base clase for Likelihood analysis Python modules.
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/users/jchiang/pythonModules/pyLikelihood/python/AnalysisBase.py,v 1.8 2005/08/19 04:11:37 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/AnalysisBase.py,v 1.6 2006/01/13 07:23:33 jchiang Exp $
 #
 
 import numarray as num
 import pyLikelihood as pyLike
+from SrcModel import SourceModel
 from SimpleDialog import SimpleDialog, map, Param
 
 _plotter_package = 'root'
 
 class AnalysisBase(object):
+    _normName = {"ConstantValue": "Value",
+                 "BrokenPowerLaw": "Prefactor",
+                 "BrokenPowerLaw2": "Integral",
+                 "PowerLaw": "Prefactor",
+                 "PowerLaw2": "Integral",
+                 "Gaussian": "Prefactor",
+                 "FileFunction": "Normalization",
+                 "LogParabola": "norm"}
     def __init__(self):
-        pass
+        self.maxdist = 20
     def _srcDialog(self):
         paramDict = map()
         paramDict['Source Model File'] = Param('file', '*.xml')
-        paramDict['optimizer'] = Param('string', 'Minuit')
+        paramDict['optimizer'] = Param('string', 'Drmngb')
         root = SimpleDialog(paramDict, title="Define Analysis Object:")
         root.mainloop()
         xmlFile = paramDict['Source Model File'].value()
         output = (xmlFile, paramDict['optimizer'].value())
         return output
-    def setPlotter(self, plotter='root'):
+    def setPlotter(self, plotter='hippo'):
         global _plotter_package
         _plotter_package = plotter
     def __call__(self):
@@ -46,6 +55,67 @@ class AnalysisBase(object):
                 self.model[i].setError(errors[j])
                 j += 1
         return errors
+    def Ts(self, srcName, reoptimize=False, approx=True):
+        self.logLike.syncParams()
+        src = self.logLike.getSource(srcName)
+        if src.getType() == "Point":
+            freeParams = pyLike.DoubleVector()
+            self.logLike.getFreeParamValues(freeParams)
+            logLike1 = self.logLike.value()
+            self._ts_src = self.logLike.deleteSource(srcName)
+            logLike0 = self.logLike.value()
+            if reoptimize:
+                myOpt = eval("self.logLike.%s()" % self.optimizer)
+                myOpt.find_min(0, 1e-5)
+            else:
+                if approx:
+                    try:
+                        self._renorm()
+                    except ZeroDivisionError:
+                        pass
+            logLike0 = max(self.logLike.value(), logLike0)
+            Ts_value = 2*(logLike1 - logLike0)
+            self.logLike.addSource(self._ts_src)
+            self.logLike.setFreeParamValues(freeParams)
+            self.model = SourceModel(self.logLike)
+            return Ts_value
+    def _renorm(self, factor=None):
+        if factor is None:
+            freeNpred, totalNpred = self._npredValues()
+            deficit = sum(self.nobs) - totalNpred
+            self.renormFactor = 1. + deficit/freeNpred
+        else:
+            self.renormFactor = factor
+        srcNames = self.sourceNames()
+        for src in srcNames:
+            parameter = self._normPar(src)
+            if parameter.isFree() and self._isDiffuseOrNearby(src):
+                oldValue = parameter.getValue()
+                parameter.setValue(oldValue*self.renormFactor)
+    def _npredValues(self):
+        srcNames = self.sourceNames()
+        freeNpred = 0
+        totalNpred = 0
+        for src in srcNames:
+            npred = self[src].Npred()
+            totalNpred += npred
+            if self._normPar(src).isFree() and self._isDiffuseOrNearby(src):
+                freeNpred += npred
+        return freeNpred, totalNpred
+    def _isDiffuseOrNearby(self, srcName):
+        if self[srcName].src.getType() == 'Diffuse':
+            return True
+        elif self._separation(self._ts_src, self[srcName].src) < self.maxdist:
+            return True
+        return False
+    def _separation(self, src1, src2):
+        dir1 = pyLike.PointSource_cast(src1).getDir()
+        dir2 = pyLike.PointSource_cast(src2).getDir()
+        return dir1.difference(dir2)*180./num.pi
+    def _normPar(self, src):
+        spectrum = self[src].spectrum()
+        funcType = spectrum.genericName()
+        return spectrum.parameter(self._normName[funcType])
     def sourceNames(self):
         srcNames = pyLike.StringVector()
         self.logLike.getSrcNames(srcNames)
@@ -117,7 +187,19 @@ class AnalysisBase(object):
         return model_counts
     def __repr__(self):
         return self._inputs
+    def __getitem__(self, name):
+        return self.model[name]
+    def __setitem__(self, name, value):
+        self.model[name] = value
     def thaw(self, i):
-        self.model[i].setFree(1)
+        try:
+            for ii in i:
+                self.model[ii].setFree(1)
+        except TypeError:
+            self.model[i].setFree(1)
     def freeze(self, i):
-        self.model[i].setFree(0)
+        try:
+            for ii in i:
+                self.model[ii].setFree(0)
+        except TypeError:
+            self.model[i].setFree(0)
