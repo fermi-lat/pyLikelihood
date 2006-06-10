2c154d3a5630ac37e1c0a470afea52be13f8de4e
cvs2git <>
2006-06-10 23:48:28 +0000

    This commit was manufactured by cvs2svn to create branch 'ScienceToolsv7r3'.
    Sprout from GLAST 2006-06-10 23:48:25 UTC jchiang 'AutoUpdate'
    Cherrypick from master 2006-06-10 23:48:26 UTC jchiang 'use optimizers::OptimizerFactory':
        cmt/requirements
        doc/images/hippo_plot_2.png
        doc/release.notes
        python/AnalysisBase.py
        python/BinnedAnalysis.py
        python/SrcModel.py
        python/UnbinnedAnalysis.py
        python/package_paths.py
        python/test_model.py
        src/pyLikelihood.in
    Delete:
        python/AnalysisBase.pyc
        python/BinnedAnalysis.pyc
        python/HippoPlot.pyc
        python/SimpleDialog.pyc
        python/SrcModel.pyc

--- python/UnbinnedAnalysis.py
+++ python/UnbinnedAnalysis.py
@@ -4,14 +4,15 @@ Python interface for unbinned likelihood
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/users/jchiang/pythonModules/pyLikelihood/python/UnbinnedAnalysis.py,v 1.6 2005/08/20 16:18:38 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/UnbinnedAnalysis.py,v 1.8 2006/04/25 01:23:12 jchiang Exp $
 #
 
+import sys
 import glob
 import numarray as num
 import pyLikelihood as pyLike
 from SrcModel import SourceModel
-from AnalysisBase import AnalysisBase
+from AnalysisBase import AnalysisBase, _quotefn
 from SimpleDialog import SimpleDialog, map, Param
 
 _funcFactory = pyLike.SourceFactory_funcFactory()
@@ -25,12 +26,16 @@ def _resolveFileList(files):
 
 class UnbinnedObs(object):
     def __init__(self, eventFile=None, scFile=None, expMap=None,
-                 expCube=None, irfs='TEST', checkCuts=True):
+                 expCube=None, irfs='DC1A', checkCuts=True, sctable='SC_DATA'):
+        self.sctable = sctable
         self.checkCuts = checkCuts
         if eventFile is None and scFile is None:
             eventFile, scFile, expMap, expCube, irfs = self._obsDialog()
         if checkCuts:
             self._checkCuts(eventFile, expMap, expCube)
+        self.expMap = expMap
+        self.expCube = expCube
+        self.irfs = irfs
         self._inputs = '\n'.join(('Event file(s): ' + str(eventFile),
                                   'Spacecraft file(s): ' + str(scFile),
                                   'Exposure map: ' + str(expMap),
@@ -39,12 +44,12 @@ class UnbinnedObs(object):
         self._respFuncs = pyLike.ResponseFunctions()
         self._respFuncs.load(irfs)
         self._expMap = pyLike.ExposureMap()
-        if expMap is not None and expMap is not "":
+        if expMap is not None and expMap != "":
             self._expMap.readExposureFile(expMap)
         self._scData = pyLike.ScData()
         self._roiCuts = pyLike.RoiCuts()
         self._expCube = pyLike.ExposureCube()
-        if expCube is not None and expCube is not "":
+        if expCube is not None and expCube != "":
             self._expCube.readExposureCube(expCube)
         self._eventCont = pyLike.EventContainer(self._respFuncs, self._roiCuts,
                                                 self._scData)
@@ -66,10 +71,10 @@ class UnbinnedObs(object):
     def _obsDialog(self):
         paramDict = map()
         paramDict['eventFile'] = Param('file', '*.fits')
-        paramDict['scFile'] = Param('file', '*scData*.fits')
+        paramDict['scFile'] = Param('file', '*.fits')
         paramDict['expMap'] = Param('file', '')
         paramDict['expCube'] = Param('file', '')
-        paramDict['irfs'] = Param('string', 'TEST')
+        paramDict['irfs'] = Param('string', 'DC1A')
         root = SimpleDialog(paramDict, title="Unbinned Analysis Elements:")
         root.mainloop()
         eventFiles = _resolveFileList(paramDict['eventFile'].value())
@@ -89,22 +94,38 @@ class UnbinnedObs(object):
         self._readEvents(eventFile)
     def _readScData(self, scFile):
         scFiles = self._fileList(scFile)
-        self._scData.readData(scFiles[0], True)
+        self._scData.readData(scFiles[0], True, self.sctable)
         for file in scFiles[1:]:
             self._scData.readData(file)
+        self.scFiles = scFiles
     def _readEvents(self, eventFile):
         if eventFile is not None:
             eventFiles = self._fileList(eventFile)
             self._roiCuts.readCuts(eventFiles, 'EVENTS', False)
             for file in eventFiles:
                 self._eventCont.getEvents(file)
+            self.eventFiles = eventFiles
     def __getattr__(self, attrname):
         return getattr(self.observation, attrname)
     def __repr__(self):
         return self._inputs
+    def state(self, output=sys.stdout):
+        close = False
+        try:
+            output = open(output, 'w')
+            close = True
+        except:
+            pass
+        output.write("from UnbinnedAnalysis import *\n")
+        output.write(("obs = UnbinnedObs(%s, %s, expMap=%s, expCube=%s, " +
+                      "irfs='%s')\n") % (`self.eventFiles`, `self.scFiles`,
+                                         _quotefn(self.expMap),
+                                         _quotefn(self.expCube), self.irfs))
+        if close:
+            output.close()
 
 class UnbinnedAnalysis(AnalysisBase):
-    def __init__(self, observation, srcModel=None,  optimizer='Minuit'):
+    def __init__(self, observation, srcModel=None,  optimizer='Drmngb'):
         AnalysisBase.__init__(self)
         if srcModel is None:
             srcModel, optimizer = self._srcDialog()
@@ -112,9 +133,10 @@ class UnbinnedAnalysis(AnalysisBase):
                                   'Source model file: ' + str(srcModel),
                                   'Optimizer: ' + str(optimizer)))
         self.observation = observation
+        self.srcModel = srcModel
         self.optimizer = optimizer
-        self.events = self.observation.eventCont().events();
         self.logLike = pyLike.LogLike(self.observation.observation)
+        self.logLike.initOutputStreams()
         self.logLike.readXml(srcModel, _funcFactory)
         self.logLike.computeEventResponses()
         self.model = SourceModel(self.logLike)
@@ -127,20 +149,26 @@ class UnbinnedAnalysis(AnalysisBase):
         self.disp = None
         self.resids = None
     def _Nobs(self):
-        nobs = []
-        for emin, emax in zip(self.energies[:-1], self.energies[1:]):
-            cnt = 0
-            for event in self.events:
-                if emin < event.getEnergy() < emax:
-                    cnt += 1
-            nobs.append(cnt)
-        return num.array(nobs, type=num.Float)
+        return num.array(self.observation.eventCont().nobs(self.energies))
     def _srcCnts(self, srcName):
         source = self.logLike.getSource(srcName)
         cnts = []
         for emin, emax in zip(self.energies[:-1], self.energies[1:]):
             cnts.append(source.Npred(emin, emax))
         return num.array(cnts)
+    def state(self, output=sys.stdout):
+        close = False
+        try:
+            output = open(output, 'w')
+            close = False
+        except:
+            pass
+        self.observation.state(output)
+        output.write(("foo = UnbinnedAnalysis(obs, srcModel=%s, " +
+                      "optimizer='%s')\n")
+                     % (_quotefn(self.srcModel), self.optimizer))
+        if close:
+            output.close()
 
 if __name__ == '__main__':
     obs = UnbinnedObs('galdiffuse_events_0000.fits',
