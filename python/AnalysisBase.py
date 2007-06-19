e62218af5e48685ae98bef408bd7a508fe25cc60
cvs2git <>
2007-06-19 16:10:16 +0000

    This commit was manufactured by cvs2svn to create tag 'v3r11p2'.
    Sprout from GLAST 2007-06-19 16:10:15 UTC jchiang 'AutoUpdate'
    Cherrypick from master 2007-06-19 16:10:03 UTC jchiang 'tagging v0r7p1':
        cmt/requirements
        doc/images/hippo_plot_2.png
        doc/release.notes
        python/AnalysisBase.py
        python/BinnedAnalysis.py
        python/SrcModel.py
        python/UnbinnedAnalysis.py
        python/package_paths.py
        python/test_model.py
        src/pyLikelihood.in
    Delete:
        python/AnalysisBase.pyc
        python/BinnedAnalysis.pyc
        python/HippoPlot.pyc
        python/SimpleDialog.pyc
        python/SrcModel.pyc

--- python/AnalysisBase.py
+++ python/AnalysisBase.py
@@ -4,40 +4,61 @@ Base clase for Likelihood analysis Python modules.
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/users/jchiang/pythonModules/pyLikelihood/python/AnalysisBase.py,v 1.8 2005/08/19 04:11:37 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/AnalysisBase.py,v 1.23 2007/05/23 15:34:13 jchiang Exp $
 #
 
 import numarray as num
 import pyLikelihood as pyLike
-from SimpleDialog import SimpleDialog, map, Param
+from SrcModel import SourceModel
+try:
+    from SimpleDialog import SimpleDialog, map, Param
+except ImportError:
+    pass
 
 _plotter_package = 'root'
 
 class AnalysisBase(object):
+    _normName = {"ConstantValue": "Value",
+                 "BrokenPowerLaw": "Prefactor",
+                 "BrokenPowerLaw2": "Integral",
+                 "PowerLaw": "Prefactor",
+                 "PowerLaw2": "Integral",
+                 "Gaussian": "Prefactor",
+                 "FileFunction": "Normalization",
+                 "LogParabola": "norm",
+                 "PLSuperExpCutoff" : "Prefactor",
+                 "ExpCutoff" : "Prefactor"}
     def __init__(self):
-        pass
+        self.maxdist = 20
+        self.tol = 1e-5
     def _srcDialog(self):
         paramDict = map()
         paramDict['Source Model File'] = Param('file', '*.xml')
-        paramDict['optimizer'] = Param('string', 'Minuit')
+        paramDict['optimizer'] = Param('string', 'Drmngb')
         root = SimpleDialog(paramDict, title="Define Analysis Object:")
         root.mainloop()
         xmlFile = paramDict['Source Model File'].value()
         output = (xmlFile, paramDict['optimizer'].value())
         return output
-    def setPlotter(self, plotter='root'):
+    def setPlotter(self, plotter='hippo'):
         global _plotter_package
         _plotter_package = plotter
     def __call__(self):
         return -self.logLike.value()
-    def fit(self, verbosity=3, tol=1e-5, optimizer=None):
+    def fit(self, verbosity=3, tol=None, optimizer=None):
+        if tol is None:
+            tol = self.tol
         errors = self._errors(optimizer, verbosity, tol)
         return -self.logLike.value()
-    def _errors(self, optimizer=None, verbosity=0, tol=1e-5, useBase=False):
+    def _errors(self, optimizer=None, verbosity=0, tol=None,
+                useBase=False):
         self.logLike.syncParams()
         if optimizer is None:
             optimizer = self.optimizer
-        myOpt = eval("self.logLike.%s()" % optimizer)
+        if tol is None:
+            tol = self.tol
+        optFactory = pyLike.OptimizerFactory_instance()
+        myOpt = optFactory.create(optimizer, self.logLike)
         myOpt.find_min(verbosity, tol)
         errors = myOpt.getUncertainty(useBase)
         j = 0
@@ -46,6 +67,87 @@ class AnalysisBase(object):
                 self.model[i].setError(errors[j])
                 j += 1
         return errors
+    def Ts(self, srcName, reoptimize=False, approx=True, tol=1e-5):
+        self.logLike.syncParams()
+        src = self.logLike.getSource(srcName)
+        if src.getType() == "Point":
+            freeParams = pyLike.DoubleVector()
+            self.logLike.getFreeParamValues(freeParams)
+            logLike1 = self.logLike.value()
+            self._ts_src = self.logLike.deleteSource(srcName)
+            logLike0 = self.logLike.value()
+            if reoptimize:
+                optFactory = pyLike.OptimizerFactory_instance()
+                myOpt = optFactory.create(self.optimizer, self.logLike)
+                myOpt.find_min(0, tol)
+            else:
+                if approx:
+                    try:
+                        self._renorm()
+                    except ZeroDivisionError:
+                        pass
+            self.logLike.syncParams()
+            logLike0 = max(self.logLike.value(), logLike0)
+            Ts_value = 2*(logLike1 - logLike0)
+            self.logLike.addSource(self._ts_src)
+            self.logLike.setFreeParamValues(freeParams)
+            self.model = SourceModel(self.logLike)
+            return Ts_value
+    def deleteSource(self, srcName):
+        src = self.logLike.deleteSource(srcName)
+        self.model = SourceModel(self.logLike)
+        return src
+    def addSource(self, src):
+        self.logLike.addSource(src)
+        self.model = SourceModel(self.logLike)
+    def writeCountsSpectra(self, outfile='counts_spectra.fits'):
+        counts = pyLike.CountsSpectra(self.logLike)
+        try:
+            emin, emax = self.observation.observation.roiCuts().getEnergyCuts()
+            counts.setEbounds(emin, emax, 21)
+        except:
+            pass
+        counts.writeTable(outfile)
+    def _renorm(self, factor=None):
+        if factor is None:
+            freeNpred, totalNpred = self._npredValues()
+            deficit = sum(self.nobs) - totalNpred
+            self.renormFactor = 1. + deficit/freeNpred
+        else:
+            self.renormFactor = factor
+        if self.renormFactor < 1:
+            self.renormFactor = 1
+        srcNames = self.sourceNames()
+        for src in srcNames:
+            parameter = self._normPar(src)
+            if parameter.isFree() and self._isDiffuseOrNearby(src):
+                oldValue = parameter.getValue()
+                parameter.setValue(oldValue*self.renormFactor)
+    def _npredValues(self):
+        srcNames = self.sourceNames()
+        freeNpred = 0
+        totalNpred = 0
+        for src in srcNames:
+#            npred = self[src].Npred()
+            npred = self.logLike.NpredValue(src)
+            totalNpred += npred
+            if self._normPar(src).isFree() and self._isDiffuseOrNearby(src):
+                freeNpred += npred
+        return freeNpred, totalNpred
+    def _isDiffuseOrNearby(self, srcName):
+        if self[srcName].src.getType() == 'Diffuse':
+            return True
+        elif self._separation(self._ts_src, self[srcName].src) < self.maxdist:
+            return True
+        return False
+    def _separation(self, src1, src2):
+        dir1 = pyLike.PointSource_cast(src1).getDir()
+        dir2 = pyLike.PointSource_cast(src2).getDir()
+        return dir1.difference(dir2)*180./num.pi
+    def _normPar(self, src):
+        spectrum = self[src].spectrum()
+        funcType = spectrum.genericName()
+        return spectrum.parameter(self._normName[funcType])
     def sourceNames(self):
         srcNames = pyLike.StringVector()
         self.logLike.getSrcNames(srcNames)
@@ -117,7 +219,37 @@ class AnalysisBase(object):
         return model_counts
     def __repr__(self):
         return self._inputs
+    def __getitem__(self, name):
+        return self.model[name]
+    def __setitem__(self, name, value):
+        self.model[name] = value
+        self.logLike.syncSrcParams(self.model[name].srcName)
     def thaw(self, i):
-        self.model[i].setFree(1)
+        try:
+            for ii in i:
+                self.model[ii].setFree(1)
+        except TypeError:
+            self.model[i].setFree(1)
     def freeze(self, i):
-        self.model[i].setFree(0)
+        try:
+            for ii in i:
+                self.model[ii].setFree(0)
+        except TypeError:
+            self.model[i].setFree(0)
+    def writeXml(self, xmlFile=None):
+        if xmlFile is None:
+            xmlFile = self.srcModel
+        self.logLike.writeXml(xmlFile)
+
+def _quotefn(filename):
+    if filename is None:
+        return None
+    else:
+        return "'" + filename + "'"
+
+def _null_file(filename):
+    if filename == 'none' or filename == '':
+        return None
+    else:
+        return filename
+
