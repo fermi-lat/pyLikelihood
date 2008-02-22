c873a06d2c283c8122deaa96e4088fb7fb6acf14
golpa <>
2008-02-22 00:28:36 +0000

    small fix to pyLikelihood library name being linked against

--- pyLikelihoodLib.py
+++ pyLikelihoodLib.py
@@ -1,5 +1,5 @@
 def generate(env, **kw):
-    env.Tool('addLibrary', library = ['pyLikelihood'], package = 'pyLikelihood')
+    env.Tool('addLibrary', library = ['_pyLikelihood'])
     env.Tool('LikelihoodLib')
 
 def exists(env):
