f2539ff574ea65afd6dd971d2ca85876f7febad1
jchiang <>
2008-08-29 19:59:34 +0000

    update UpperLimits.py with pyASP version that does quadratic fit and extrapolation for likelihood surface

--- python/UpperLimits.py
+++ python/UpperLimits.py
@@ -6,11 +6,36 @@
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/UpperLimits.py,v 1.1 2008/03/28 22:58:25 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/ASP/pyASP/python/UpperLimits.py,v 1.1 2008/05/13 05:13:13 jchiang Exp $
 #
 import pyLikelihood as pyLike
 import numpy as num
 
+class QuadraticFit(object):
+    def __init__(self, xx, yy):
+        self.Sx = self.Sy = self.Sxy = self.Sxx = self.npts = 0
+        self.xmin = min(xx)
+        for x, y in zip(xx, yy):
+            self.add_pair(x, y)
+    def add_pair(self, x, y):
+        xx = (x-self.xmin)*(x-self.xmin)
+        self.Sx += xx
+        self.Sy += y
+        self.Sxy += xx*y
+        self.Sxx +=  xx*xx
+        self.npts += 1
+    def fitPars(self):
+        denominator = self.npts*self.Sxx - self.Sx*self.Sx
+        slope = (self.npts*self.Sxy - self.Sy*self.Sx)/denominator
+        intercept = (self.Sxx*self.Sy - self.Sx*self.Sxy)/denominator
+        return intercept, slope
+    def xval(self, yval):
+        y0, dydx = self.fitPars()
+        return num.sqrt((yval - y0)/dydx) + self.xmin
+    def yval(self, xval):
+        y0, dydx = self.fitPars()
+        return y0 + dydx*xval*xval
+    
 class ULResult(object):
     def __init__(self, value, emin, emax, delta, fluxes, dlogLike, parvalues):
         self.value = value
@@ -28,7 +53,7 @@ class UpperLimit(object):
         self.results = []
     def compute(self, emin=100, emax=3e5, delta=2.71/2., 
                 tmpfile='temp_model.xml', fix_src_pars=False,
-                verbose=True, nsigmax=5, npts=30, renorm=False):
+                verbose=True, nsigmax=2, npts=5, renorm=False):
         source = self.source
         saved_pars = [par.value() for par in self.like.model.params]
 
@@ -60,6 +85,9 @@ class UpperLimit(object):
         xvals, dlogLike, fluxes = [], [], []
         if verbose:
             print self.like.model
+        #
+        # Fit a quadratic to a handful of points
+        #
         for i, x in enumerate(num.arange(x0, x0+nsigmax*dx, nsigmax*dx/npts)):
             xvals.append(x)
             par.setValue(x)
@@ -69,12 +97,22 @@ class UpperLimit(object):
             fluxes.append(self.like[source].flux(emin, emax))
             if verbose:
                 print i, x, dlogLike[-1], fluxes[-1]
-            if dlogLike[-1] > delta:
-                break
-            if len(dlogLike) > 2 and dlogLike[-1] < dlogLike[-2]:
-                xvals.pop()
-                dlogLike.pop()
-                break
+        yfit = QuadraticFit(xvals, dlogLike)
+        #
+        # Extend the fit until it surpasses the desired delta
+        #
+        while dlogLike[-1] < delta and len(dlogLike) < 30:
+            x = yfit.xval(1.1*delta)
+            xvals.append(x)
+            par.setValue(x)
+            self.like.logLike.syncSrcParams(source)
+            self.fit(0, renorm=renorm)
+            dlogLike.append(self.like() - logLike0)
+            fluxes.append(self.like[source].flux(emin, emax))
+            yfit.add_pair(x, dlogLike[-1])
+            i += 1
+            if verbose:
+                print i, x, dlogLike[-1], fluxes[-1]
         par.setFree(1)
         if fix_src_pars:
             for item in freePars:
@@ -139,10 +177,13 @@ class UpperLimits(dict):
         for srcName in like.sourceNames():
             if self.like.logLike.getSource(srcName).getType() == "Point":
                 self[srcName] = UpperLimit(like, srcName)
-    
+
 if __name__ == '__main__':
-    from UnbinnedAnalysis import *
-    like = unbinnedAnalysis(mode='h')
+    import hippoplotter as plot
+    from analysis import like
     ul = UpperLimits(like)
-    ul['grb'].compute()
-    print ul['grb'].results
+    print ul['point source 0'].compute(renorm=True)
+    results = ul['point source 0'].results[0]
+    plot.scatter(results.parvalues, results.dlogLike, xname='par value',
+                 yname='dlogLike')
+    plot.hline(2.71/2)
