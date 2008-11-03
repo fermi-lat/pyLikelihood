825fc1a7161a64df5aa4f159deae1b8e4183bd6a
jchiang <>
2008-11-03 16:40:25 +0000

    check in David Landriu's changes

--- python/RootPlot.py
+++ python/RootPlot.py
@@ -4,14 +4,13 @@ Standard plotting interface for XY plots with pyROOT backend
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/RootPlot.py,v 1.3 2008/09/02 19:50:22 jchiang Exp $
+# $Header$
 #
-
 import sys
 from array import array
 import numpy as num
     
-from ROOT import TCanvas, TGraphErrors, TH2F
+from ROOT import TCanvas, TGraphErrors, TH2F, gStyle
 
 _ncanvas = -1
 
@@ -21,10 +20,12 @@ _symbols = {'line' : ('C', 0, 1),
             'dash-dot' : ('C', 0, 4),
             'poly-line' : ('L',0,1),
             'point' : ('P', 0, 1),
+            'nopoint' : ('P',1,1),
             'plus' : ('P', 2, 1),
             'times' : ('P', 3, 1)}
 
-_colors = {'black' : 1,
+_colors = {'white' : 0,
+           'black' : 1,
            'red' : 2,
            'green' : 3,
            'blue' : 4,
@@ -32,15 +33,18 @@ _colors = {'black' : 1,
            'magenta' : 6,
            'cyan' : 7}
 
+gStyle.SetPadLeftMargin(.20)
+        
 class RootPlot(object):
     def __init__(self, x, y, dx=None, dy=None, xlog=0, ylog=0,
-                 xtitle="x", ytitle="y", symbol='plus', color='black',
-                 xrange=None, yrange=None):
+                 xtitle="x", ytitle="y", MainTitle='', symbol='plus',
+                 color='black', xrange=None, yrange=None):
         self._createCanvas(xlog, ylog)
         self._createBaseGraph(x, y, dx, dy)
         self._drawAxes(xrange, yrange)
-        self._setTitles(xtitle, ytitle)
+        self._setTitles(xtitle, ytitle, MainTitle)
         self._drawData(self.graphs[0], symbol, color)
+
     def _createCanvas(self, xlog, ylog):
         global _ncanvas
         _ncanvas += 1
@@ -63,12 +67,13 @@ class RootPlot(object):
                          10, yrange[0], yrange[1])
         self.hist.SetStats(0)
         self.hist.Draw()
-    def _setTitles(self, xtitle, ytitle):
+    def _setTitles(self, xtitle, ytitle, MainTitle):
         self.hist.GetXaxis().SetTitle(xtitle)
         self.hist.GetXaxis().CenterTitle()
         self.hist.GetYaxis().SetTitle(ytitle)
         self.hist.GetYaxis().CenterTitle()
-        self.hist.SetTitle('')
+        self.hist.GetYaxis().SetTitleOffset(2.)
+        self.hist.SetTitle(MainTitle)
     def _drawData(self, graph, key, color):
         symbol, marker, lineStyle = _symbols[key]
         if symbol in ['C','L']:
