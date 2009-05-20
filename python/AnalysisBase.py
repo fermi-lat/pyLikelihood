70e34dcf2a19005419f5d720daa18c8ce21ec6ab
jchiang <>
2009-05-20 14:44:16 +0000

    ensure the extra attributes are restored after rebuilding the SourceModel object

--- python/AnalysisBase.py
+++ python/AnalysisBase.py
@@ -4,7 +4,7 @@ Base clase for Likelihood analysis Python modules.
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/AnalysisBase.py,v 1.45 2009/03/31 18:20:53 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/AnalysisBase.py,v 1.46 2009/05/15 04:15:28 jchiang Exp $
 #
 
 import sys
@@ -183,12 +183,23 @@ class AnalysisBase(object):
 
         return num.sqrt(num.dot(partials, num.dot(my_covar, partials)))
     def deleteSource(self, srcName):
+        source_attributes = self.getExtraSourceAttributes()
         src = self.logLike.deleteSource(srcName)
         self.model = SourceModel(self.logLike)
+        #
+        # reset the remaining extra source attributes
+        #
+        for item in source_attributes:
+            if self.model[item] is not None:
+                self.model[item].__dict__.update(source_attributes[item])
         return src
     def addSource(self, src):
+        source_attributes = self.getExtraSourceAttributes()
         self.logLike.addSource(src)
         self.model = SourceModel(self.logLike)
+        for item in source_attributes:
+            if self.model[item] is not None:
+                self.model[item].__dict__.update(source_attributes[item])
     def writeCountsSpectra(self, outfile='counts_spectra.fits'):
         counts = pyLike.CountsSpectra(self.logLike)
         try:
