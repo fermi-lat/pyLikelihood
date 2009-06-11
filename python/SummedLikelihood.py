0c9ab8d2ad3290f4f24c33a10666d26650d86a05
jchiang <>
2009-06-11 19:06:55 +0000

    enable Ts calculation

--- python/SummedLikelihood.py
+++ python/SummedLikelihood.py
@@ -1,14 +1,16 @@
 """
 @brief Wrapper interface for pyLikelihood.SummedLikelihood to provide
-more natural symantics for use in python alongside other analysis classes.
+more natural symantics for use in python alongside other analysis
+classes.
 
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/SummedLikelihood.py,v 1.5 2009/06/08 22:57:33 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/SummedLikelihood.py,v 1.6 2009/06/08 23:09:03 jchiang Exp $
 #
 
 import pyLikelihood as pyLike
+from SrcModel import SourceModel
 
 class Parameter(object):
     "Composite parameter object."
@@ -110,8 +112,15 @@ class SummedLikelihood(object):
     def __getitem__(self, name):
         return self.model[name]
     def __setitem__(self, name, value):
-        self.model[name] = value
-        self.composite.syncSrcParams(self.model[name].srcName)
+        for component in self.components:
+            component[name] = value
+            component.syncSrcParams(self.model[name].srcName)
+    def thaw(self, i):
+        for component in self.components:
+            component.thaw(i)
+    def freeze(self, i):
+        for component in self.components:
+            component.freeze(i)
     def _errors(self, optimizer=None, verbosity=0, tol=None,
                 useBase=False, covar=False, optObject=None):
         self._syncParams()
@@ -139,3 +148,81 @@ class SummedLikelihood(object):
                     component.model[i].setError(errors[j])
                     j += 1
         return errors
+    def Ts(self, srcName, reoptimize=False, approx=True,
+           tol=None, MaxIterations=10, verbosity=0):
+        if verbosity > 0:
+            print "*** Start Ts_dl ***"
+        source_attributes = self.components[0].getExtraSourceAttributes()
+        self.syncSrcParams()
+        freeParams = pyLike.DoubleVector()
+        self.components[0].logLike.getFreeParamValues(freeParams)
+        logLike1 = -self()
+        self._ts_src = []
+        for comp in self.components:
+            comp._ts_src = comp.logLike.deleteSource(srcName) 
+            self._ts_src.append(comp._ts_src)
+        logLike0 = -self()
+        if tol is None:
+            tol = self.tol
+        if reoptimize:
+            if verbosity > 0:
+                print "** Do reoptimize"
+            optFactory = pyLike.OptimizerFactory_instance()
+            myOpt = optFactory.create(self.optimizer, self.composite)
+            Niter = 1
+            while Niter <= MaxIterations:
+                try:
+                    myOpt.find_min(0, tol)
+                    break
+                except RuntimeError,e:
+                    print e
+                if verbosity > 0:
+                    print "** Iteration :",Niter
+                Niter += 1
+        else:
+            if approx:
+                try:
+                    self._renorm()
+                except ZeroDivisionError:
+                    pass
+        self.syncSrcParams()
+        logLike0 = max(-self(), logLike0)
+        Ts_value = 2*(logLike1 - logLike0)
+        for ts_src, comp in zip(self._ts_src, self.components):
+            comp.logLike.addSource(ts_src)
+            comp.logLike.setFreeParamValues(freeParams)
+            comp.model = SourceModel(comp.logLike)
+            for src in source_attributes:
+                comp.model[src].__dict__.update(source_attributes[src])
+        return Ts_value
+    def _renorm(self, factor=None):
+        if factor is None:
+            freeNpred, totalNpred = self._npredValues()
+            deficit = self.total_nobs() - totalNpred
+            self.renormFactor = 1. + deficit/freeNpred
+        else:
+            self.renormFactor = factor
+        if self.renormFactor < 1:
+            self.renormFactor = 1
+        srcNames = self.sourceNames()
+        for src in srcNames:
+            parameter = self.normPar(src)
+            if (parameter.isFree() and 
+                self.components[0]._isDiffuseOrNearby(src)):
+                oldValue = parameter.getValue()
+                newValue = oldValue*self.renormFactor
+                # ensure new value is within parameter bounds
+                xmin, xmax = parameter.getBounds()
+                if xmin <= newValue and newValue <= xmax:
+                    parameter.setValue(newValue)
+    def _npredValues(self):
+        srcNames = self.sourceNames()
+        freeNpred = 0
+        totalNpred = 0
+        for src in srcNames:
+            npred = self.NpredValue(src)
+            totalNpred += npred
+            if (self.normPar(src).isFree() and 
+                self.components[0]._isDiffuseOrNearby(src)):
+                freeNpred += npred
+        return freeNpred, totalNpred
