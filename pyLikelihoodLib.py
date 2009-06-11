163aaf05566764addcba3facdde6298bb430e1ca
cvs2git <>
2009-06-11 21:31:05 +0000

    This commit was manufactured by cvs2svn to create branch 'ScienceTools-09-15-02-swigFix'.
    Sprout from master 2009-06-11 21:31:03 UTC glastrm 'Updated version number in SConscript file'
    Delete:
        .cvsignore
        ChangeLog
        cmt/requirements
        doc/Doxyfile
        doc/Makefile
        doc/images/Analysis_dialog.png
        doc/images/Obs_dialog0.png
        doc/images/Obs_dialog1.png
        doc/images/hippo_plot.png
        doc/images/hippo_plot_2.png
        doc/images/pyROOT_plot.png
        doc/release.notes
        pyLikelihood/Aeff.h
        pyLikelihood/enableFPE.h
        pyLikelihoodLib.py
        python/AnalysisBase.py
        python/BinnedAnalysis.py
        python/CompositeLikelihood.py
        python/HippoPlot.py
        python/RootPlot.py
        python/SimpleDialog.py
        python/SrcAnalysis.py
        python/SrcModel.py
        python/SummedLikelihood.py
        python/UnbinnedAnalysis.py
        python/UpperLimits.py
        python/package_paths.py
        python/test_model.py
        src/mainpage.h
        src/pyLikelihood.h
        src/pyLikelihood.i
        src/pyLikelihood.in

--- pyLikelihoodLib.py
+++
@@ -1,8 +0,0 @@
-#$Id$
-def generate(env, **kw):
-    if not kw.get('depsOnly',0):
-        env.Tool('addLibrary', library = ['_pyLikelihood'])
-    env.Tool('LikelihoodLib')
-
-def exists(env):
-    return 1
