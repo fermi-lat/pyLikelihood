eac3e7082f6a66b094bf0b2f316668d1a34a8caa
jperkins <>
2013-04-19 15:52:49 +0000

    Added docStrings to all public functions in AnalysisBase.py

--- python/AnalysisBase.py
+++ python/AnalysisBase.py
@@ -4,7 +4,7 @@ Base class for Likelihood analysis Python modules.
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/pyLikelihood/python/AnalysisBase.py,v 1.75 2012/05/18 17:46:24 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pyLikelihood/python/AnalysisBase.py,v 1.76 2012/12/11 22:21:57 kadrlica Exp $
 #
 
 import sys
@@ -47,6 +47,10 @@ class AnalysisBase(object):
     def __call__(self):
         return -self.logLike.value()
     def setFitTolType(self, tolType):
+
+        '''Set the tolerance type of the fit.  Valid values of
+        "tolType" are "0" for RELATIVE and "1" for ABSOLUTE.'''
+        
         if tolType in (pyLike.RELATIVE, pyLike.ABSOLUTE):
             self.tolType = tolType
         else:
@@ -54,6 +58,16 @@ class AnalysisBase(object):
                                "Valid values are 0=RELATIVE or 1=ABSOLUTE")
     def fit(self, verbosity=3, tol=None, optimizer=None,
             covar=False, optObject=None):
+
+        '''Perform the likelihood fit.  If "tol" is set to "None" it
+        reverts to the global tolerance.  If "optimizer" is set to
+        "None" it reverts to the global optimizer.  You can
+        selectively calculate the covariance matrix by setting
+        "covar=True".  You can also provide an optimization object via
+        "optObject = myOpt" if you would like access to the optimizer
+        results like the return codes after the fit (see pyLike for
+        more details).  This function returns -Log(like).'''
+        
         if tol is None:
             tol = self.tol
         errors = self._errors(optimizer, verbosity, tol, covar=covar,
@@ -61,6 +75,17 @@ class AnalysisBase(object):
         return -self.logLike.value()
     def optimize(self, verbosity=3, tol=None, optimizer=None,
                  optObject=None):
+
+        '''Optimize the current model.  You can increase the verbosity
+        of the optimizer using the verbosity option (default is 3).
+        If "tol" is none, it will use the global tolerance.  If
+        "optimizer" is none it will use the global optimizer.  If
+        "optObject" is none it will create one.  It will not replace
+        the global optObject (self.optObject) with this one if it
+        exists but if it is none, it will replace it with the new one.
+        This function is different from the "fit" routine in that it
+        does not calculate the covarience matrix.'''
+
         self.logLike.syncParams()
         if optimizer is None:
             optimizer = self.optimizer
@@ -103,6 +128,22 @@ class AnalysisBase(object):
                 j += 1
         return errors
     def minosError(self, *args):
+
+        '''Evaluate the minos errors for a Minuit or NewMinuit fit.
+        There are several ways to call this function:
+        
+           minosError(index): pass the parameter index only
+           minosError(index,level): pass the parameter index and the
+                                    level
+           minosError(srcName,parName): pass the source name and
+                                        the parameter name
+
+        The "level" parameter is used to dynamically set a new error
+        definition.  The default is suitable for a -log(like) analysis
+        (1, sets the minuit level to 0.5, see
+        Minuit2::FCNBase::ErrorDef).  Setting this to 2 would be
+        suitable for a chi^2 analysis.'''
+        
         if len(args) == 1:
             #param passed by index, no level
             return self._minosIndexError(args[0])
@@ -115,6 +156,11 @@ class AnalysisBase(object):
         else:
             return self._minosIndexError(par_index, args[2])
     def par_index(self, srcName, parName):
+
+        '''Returns the parameter index number in the model of the
+        parameter identified by "parName" for a specific source
+        identified by "srcName".'''
+        
         source_names = self.sourceNames()
         par_index = -1
         for src in source_names:
@@ -156,6 +202,11 @@ class AnalysisBase(object):
             self.logLike.setFreeParamValues(freeParams)
             raise RuntimeError(message)
     def getExtraSourceAttributes(self):
+
+        '''Returns all of the extra attributes of all of the sources
+        in the model.  These are any attributes that are not "funcs"
+        or "src" which includes things like "name" and "type".'''
+        
         source_attributes = {}
         for src in self.model.srcNames:
             source_attributes[src] = {}
@@ -165,6 +216,14 @@ class AnalysisBase(object):
         return source_attributes
     def Ts(self, srcName, reoptimize=False, approx=True,
            tol=None, MaxIterations=10, verbosity=0):
+
+        '''Computes the TS value for a source indicated by "srcName."
+        If "reoptimize=True" is selected this function will reoptimize
+        the model up to "MaxIterations" given the tolerance "tol"
+        (default is the tolerance selected for the overall fit).  If
+        "appox=True" is selected (the default) it will renormalize the
+        model (see _renorm).'''
+        
         saved_state = LikelihoodState(self)
         if verbosity > 0:
             print "*** Start Ts_dl ***"
@@ -211,6 +270,14 @@ class AnalysisBase(object):
         self.logLike.value()
         return Ts_value
     def Ts_old(self, srcName, reoptimize=False, approx=True, tol=None):
+
+        '''NOTE: this is the old method. Computes the TS value for a
+        source indicated by "srcName."  If "reoptimize=True" is
+        selected this function will reoptimize the model given the
+        tolerance "tol" (default is the tolerance selected for the
+        overall fit).  If "appox=True" is selected (the default) it
+        will renormalize the model (see _renorm).'''
+        
         source_attributes = self.getExtraSourceAttributes()
         self.logLike.syncParams()
         src = self.logLike.getSource(srcName)
@@ -244,16 +311,38 @@ class AnalysisBase(object):
             self.model[src].__dict__.update(source_attributes[src])
         return Ts_value
     def flux(self, srcName, emin=100, emax=3e5, energyFlux=False):
+
+        '''Returns the flux for a source with name "srcName" from emin
+        (in MeV) to emax (in MeV).  If "energyFlux=False" it returns
+        the integral flux; if True it will return the differential
+        flux.'''
+
         if energyFlux:
             return self[srcName].energyFlux(emin, emax)
         else:
             return self[srcName].flux(emin, emax)
     def energyFlux(self, srcName, emin=100, emax=3e5):
+
+        '''Returns the differential flux for a source with name
+        "srcName" bewtween emin and emax (in MeV).'''
+
         return self.flux(srcName, emin, emax, True)
     def energyFluxError(self, srcName, emin=100, emax=3e5, npts=1000):
+
+        '''Returns the error on the differential flux for a source
+        with name "srcName" between emin and emax (in MeV).  "npts" is
+        the number of energy points to use in the error calculation.
+        '''
+
         return self.fluxError(srcName, emin, emax, True, npts)
     def fluxError(self, srcName, emin=100, emax=3e5, energyFlux=False,
                   npts=1000):
+
+        '''Returns the error on the flux for a source with name
+        "srcName" from emin (in MeV) to emax (in MeV).  If
+        "energyFlux=False" it returns the integral flux; if True it
+        will return the differential flux.'''
+        
         par_index_map = {}
         indx = 0
         for src in self.sourceNames():
@@ -293,17 +382,54 @@ class AnalysisBase(object):
 
         return num.sqrt(num.dot(partials, num.dot(my_covar, partials)))
     def setSpectrum(self, srcName, functionName):
+
+        '''Set the spectral shape of a source identified by "srcName".
+        The "functionName" is a string that must be one of the
+        avaialble spectral functions (i.e. PowerLaw, PowerLaw2 etc.).
+        You can also pass a spectrum object (see the addSource
+        docString for more details).  The spectral parameters of this
+        new model will be set to defaults which are probably not what
+        you want them to be.'''
+        
         source_attributes = self.getExtraSourceAttributes()
         src = self.logLike.getSource(srcName)
         src.setSpectrum(functionName)
         self.syncSrcParams(srcName)
         self._setSourceAttributes(source_attributes)
     def deleteSource(self, srcName):
+
+        '''Removes a source with name "srcName" from the model.  It
+        returns this source object so you can save it and use it
+        later.'''
+        
         source_attributes = self.getExtraSourceAttributes()
         src = self.logLike.deleteSource(srcName)
         self._setSourceAttributes(source_attributes)
         return src
     def addSource(self, src):
+
+        '''Add a source to the active model.  You should pass a source
+        object to this function.  Example:
+
+        > test_src = pyLike.PointSource(0, 0, like.observation.observation)
+        > pl = pyLike.SourceFactory_funcFactory().create("PowerLaw")
+        > pl.setParamValues((1, -2, 100))
+        > indexPar = pl.getParam("Index")
+        > indexPar.setBounds(-3.5, -1)
+        > pl.setParam(indexPar)
+        > prefactor = pl.getParam("Prefactor")
+        > prefactor.setBounds(1e-10, 1e3)
+        > prefactor.setScale(1e-9)
+        > pl.setParam(prefactor)
+        > test_src.setSpectrum(pl)
+        > test_src.setName("testSource")
+        > test_src.setDir(193.45,-5.83,True,False)
+        > like.addSource(test_src)
+
+        You could also use the delete source function to return a
+        fully formed source object and modify the parameters of that.
+        '''
+        
         source_attributes = self.getExtraSourceAttributes()
         self.logLike.addSource(src)
         self._setSourceAttributes(source_attributes)
@@ -313,6 +439,12 @@ class AnalysisBase(object):
             if self.model[item] is not None:
                 self.model[item].__dict__.update(source_attributes[item])
     def writeCountsSpectra(self, outfile='counts_spectra.fits'):
+
+        '''Writes a FITS file with the name "outfile" that contains
+        the counts spectra of all of the sources in the active model.
+        This is the same file that is written out by the balistic
+        gtlike program.'''
+        
         counts = pyLike.CountsSpectra(self.logLike)
         try:
             emin, emax = self.observation.observation.roiCuts().getEnergyCuts()
@@ -361,20 +493,39 @@ class AnalysisBase(object):
         dir2 = pyLike.PointSource_cast(src2).getDir()
         return dir1.difference(dir2)*180./num.pi
     def saveCurrentFit(self):
+
+        '''Saves the active fit parameter values if this fit happens
+        to have a better log likelihood value than the one currently
+        saved.  If it does, it replaces the saved fit with the active
+        one.'''
+        
         self.logLike.saveCurrentFit()
     def restoreBestFit(self):
+
+        '''Restores the previously saved fit values saved via the
+        "saveCurrentFit" function.'''
+        
         self.logLike.restoreBestFit()
     def NpredValue(self, src):
+        '''Returns the number of predicted counts for a source.'''
         return self.logLike.NpredValue(src)
     def total_nobs(self):
+        '''Returns the total number of observed counts in the RoI.'''
         return sum(self.nobs)
     def syncSrcParams(self, src=None):
+
+        '''Synchronizes the parameters of a source (identified by
+        "src") with the active model.  This is useful if you add or
+        delete a source from the model and want to reevaluate the log
+        likelihood values.'''
+        
         if src is not None:
             self.logLike.syncSrcParams(src)
         else:
             for src in self.sourceNames():
                 self.logLike.syncSrcParams(src)
     def sourceNames(self):
+        '''Returns a tuple that contains all of the source names in the model'''
         srcNames = pyLike.StringVector()
         self.logLike.getSrcNames(srcNames)
         return tuple(srcNames)
@@ -482,30 +633,65 @@ class AnalysisBase(object):
         self.model[name] = value
         self.logLike.syncSrcParams(self.model[name].srcName)
     def normPar(self, srcName):
+
+        '''Returns the normalization paramter of a souce specified by
+        "srcName" (for example, it will return the "Prefactor"
+        parameter of a source described by a PowerLaw).'''
+        
         return self[srcName].funcs['Spectrum'].normPar()
     def freePars(self, srcName):
+
+        '''Returns a tuple that contains all of the free parameters
+        for a specific source indicated by "srcName".'''
+        
         pars = pyLike.ParameterVector()
         self[srcName].funcs['Spectrum'].getFreeParams(pars)
         return tuple([x for x in pars])
     def setFreeFlag(self, srcName, pars, value):
+
+        '''Sets the free flag to free (value = 1) or frozen (value =
+        0) for the list of parameters indicated by pars for a specific
+        source indicated by srcName.  '''
+
+        
         src_spectrum = self[srcName].funcs['Spectrum']
         for item in pars:
             src_spectrum.parameter(item.getName()).setFree(value)
     def params(self):
+
+        '''Returns a list of all of the parameters in the active
+        model.'''
+        
         return self.model.params
     def thaw(self, i):
+
+        '''Thaws a parameter with the given parameter index.  Use the
+        par_index function to determine the index number of a specific
+        parameter of a specific source.'''
+        
         try:
             for ii in i:
                 self.model[ii].setFree(1)
         except TypeError:
             self.model[i].setFree(1)
     def freeze(self, i):
+
+        '''Freezes a parameter with the given parameter index.  Use
+        the par_index function to determine the index number of a
+        specific parameter of a specific source.'''
+        
         try:
             for ii in i:
                 self.model[ii].setFree(0)
         except TypeError:
             self.model[i].setFree(0)
     def writeXml(self, xmlFile=None):
+
+        '''Write out an xml representation of the active model with
+        the filename xmlFile.  If xmlFile is None (the default) it
+        will overwrite the global xml file (i.e. the original file).
+        '''
+        
         if xmlFile is None:
             xmlFile = self.srcModel
         self.logLike.writeXml(xmlFile)
@@ -513,6 +699,18 @@ class AnalysisBase(object):
     def scan(self, srcName, parName, xmin=0, xmax=10, npts=50,
              tol=None, optimizer=None, optObject=None,
              fix_src_pars=False, verbosity=0, renorm=False):
+
+        '''This function scans the values of a specific parameter
+        specified by "parName" of a specific source specified by
+        "srcName" in the range [xmin:xmax] with a resolution of npts.
+        It then optimizes the active model.  This function returns two
+        arrays: the first is an array of the x values evaluated and
+        the second is an array of the change in likelihood value from
+        the original model at those x values.  You can also pass True
+        to "fix_src_pars" to fix all of the parameters of the source
+        of interest.  The other options are similar to the fit and
+        optimize functions. '''
+        
         saved_state = LikelihoodState(self)
         indx = self.par_index(srcName,parName)
         # Fix the normalization parameter for the scan.
