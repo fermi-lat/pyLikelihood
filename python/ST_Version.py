f2d4f554222cf7536b0e4815f049cf240c91e497
echarles <>
2016-09-13 19:35:43 +0000

    Fixed ST_Version

--- python/ST_Version.py
+++ python/ST_Version.py
@@ -3,6 +3,6 @@ Some utilites, for now this is only the verysion
 """
 
 def version():
-    the_version = "$Name: ScienceTools-11-03-00$"
-    return the_version[7:-1]
+    the_version = "$Name: pyLikelihood-02-03-03 $"
+    return the_version[7:-2]
 
